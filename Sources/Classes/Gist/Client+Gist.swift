4b266ec21e198a6b29cdaa63093805d20a328e0f
Khoa Pham <onmyway133@gmail.com>
2016-04-20 23:52:59 +0200

    fetchGists

--- Sources/Classes/Gist/Client+Gist.swift
+++ Sources/Classes/Gist/Client+Gist.swift
@@ -10,5 +10,29 @@ import Foundation
 import RxSwift
 
 public extension Client {
-  
+
+  // Fetches the gists since the date for the current user.
+  //
+  // since - If not nil, only gists updated at or after this time are returned.
+  //         If nil, all the gists are returned.
+  //
+  // Returns a signal which will send zero or more OCTGists and complete. If the client
+  // is not `authenticated`, the signal will error immediately.
+  public func fetchGists(updatedSince: NSDate? = nil) -> Observable<[Gist]> {
+    if !isAuthenticated {
+      return Observable<[Gist]>.error(Error.authenticationRequiredError())
+    }
+
+    let requestDescriptor = RequestDescriptor().then {
+      $0.path = "gists"
+
+      if let since = updatedSince {
+        $0.parameters["since"] = Formatter.string(date: since)
+      }
+    }
+
+    return enqueue(requestDescriptor).map {
+      return Parser.all($0.jsonArray)
+    }
+  }
 }
