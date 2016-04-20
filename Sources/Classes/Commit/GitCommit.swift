81e0392d4b7be20d031efd7cbd436edceded1a40
Khoa Pham <onmyway133@gmail.com>
2016-04-20 21:06:01 +0200

    Add GitCommit, GitCommitFile

--- Sources/Classes/Commit/GitCommit.swift
+++ Sources/Classes/Commit/GitCommit.swift
@@ -13,7 +13,68 @@ import Sugar
 // A git commit.
 public class GitCommit: Object {
 
+  // The commit URL for this commit.
+  public private(set) var commitURL: NSURL?
+
+  // The commit message for this commit.
+  public private(set) var message: String = ""
+
+  // The SHA for this commit.
+  public private(set) var SHA: String = ""
+
+  // The committer of this commit.
+  public private(set) var committer: User?
+
+  // The author of this commit.
+  public private(set) var author: User?
+
+  // The date the author signed the commit.
+  public private(set) var commitDate: NSDate?
+
+  // The number of changes made in the commit.
+  // This property is only set when fetching a full commit.
+  public private(set) var  countOfChanges: Int = 0
+
+  // The number of additions made in the commit.
+  // This property is only set when fetching a full commit.
+  public private(set) var countOfAdditions: Int = 0
+
+  // The number of deletions made in the commit.
+  // This property is only set when fetching a full commit.
+  public private(set) var countOfDeletions: Int = 0
+
+  // The OCTGitCommitFile objects changed in the commit.
+  // This property is only set when fetching a full commit.
+  public private(set) var files: [GitCommitFile] = []
+
+  // The authors git user.name property. This is only useful if the
+  // author does not have a GitHub login. Otherwise, author should
+  // be used.
+  public private(set) var authorName: String = ""
+
+  // The committer's git user.name property. This is only useful if the
+  // committer does not have a GitHub login. Otherwise, committer should
+  // be used.
+  public private(set) var committerName: String = ""
+
   public required init(_ map: JSONDictionary) {
     super.init(map)
+
+    let commit = map.dictionary("commit")
+    let author = commit?.dictionary("author")
+    let stats = map.dictionary("stats")
+
+    self.commitURL <- map.transform("url", transformer: Transformer.stringToURL)
+    self.message <- commit?.property("message")
+    self.SHA <- map.property("sha")
+    self.committer <- map.relation("commiter")
+    self.author <- map.relation("author")
+    self.commitDate <- author?.transform("date", transformer: Transformer.stringToDate)
+    self.countOfAdditions <- stats?.property("additions")
+    self.countOfDeletions <- stats?.property("deletions")
+    self.countOfChanges <- stats?.property("total")
+    self.files <- map.relations("files")
+    self.authorName <- author?.property("name")
+    self.committerName <- commit?.dictionary("committer")?.property("name")
   }
 }
---
+++ Sources/Classes/Commit/GitCommitFile.swift
@@ -0,0 +1,51 @@
+//
+//  GitCommitFile.swift
+//  GithubSwift
+//
+//  Created by Khoa Pham on 20/04/16.
+//  Copyright © 2016 Fantageek. All rights reserved.
+//
+
+import Foundation
+import Tailor
+import Sugar
+
+// A file of a commit
+public class GitCommitFile: Object {
+
+  // The filename in the repository.
+  public private(set) var fileName: String = ""
+
+  // The number of additions made in the commit.
+  public private(set) var countOfAdditions: Int = 0
+
+  // The number of deletions made in the commit.
+  public private(set) var countOfDeletions: Int = 0
+
+  // The number of changes made in the commit.
+  public private(set) var countOfChanges: Int = 0
+
+  // The status of the commit, e.g. 'added' or 'modified'.
+  public private(set) var status: String = ""
+
+  // The GitHub URL for the whole file.
+  public private(set) var rawURL: NSURL?
+
+  // The GitHub blob URL.
+  public private(set) var blobURL: NSURL?
+
+  // The patch on this file in a commit.
+  public private(set) var patch: String = ""
+
+  public required init(_ map: JSONDictionary) {
+    super.init(map)
+
+    self.fileName <- map.property("filename")
+    self.countOfAdditions <- map.property("additions")
+    self.countOfDeletions <- map.property("deletions")
+    self.countOfChanges <- map.property("changes")
+    self.status <- map.property("status")
+    self.rawURL <- map.transform("raw_url", transformer: Transformer.stringToURL)
+    self.blobURL <- map.transform("blob_url", transformer: Transformer.stringToURL)
+  }
+}
