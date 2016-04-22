590cafa344bd3bb812cf04d0fe20bb27b9fa2dbc
Khoa Pham <onmyway133@gmail.com>
2016-04-23 00:50:14 +0200

    fetchTree

--- Sources/Classes/Git/Client+Git.swift
+++ Sources/Classes/Git/Client+Git.swift
@@ -11,4 +11,25 @@ import RxSwift
 
 public extension Client {
 
+  // Fetches the tree for the given reference.
+  //
+  // reference  - The SHA, branch, reference, or tag to fetch. May be nil, in
+  //              which case HEAD is fetched.
+  // repository - The repository from which the tree should be fetched. Cannot be
+  //              nil.
+  // recursive  - Should the tree be fetched recursively?
+  //
+  // Returns a signal which will send an OCTTree and complete or error.
+  public func fetchTree(reference: String = "HEAD", repository: Repository, recursive: Bool = false) -> Observable<Tree> {
+    let requestDescriptor = RequestDescriptor().then {
+      $0.path = "repos/\(repository.ownerLogin)/\(repository.name)/git/trees/\(reference)"
+      if recursive {
+        $0.parameters["recursive"] = true
+      }
+    }
+
+    return enqueue(requestDescriptor).map {
+      return Parser.one($0.jsonArray)
+    }
+  }
 }
